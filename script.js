f53ca797cd3126e18271cced0388921c6098fcf2
Santiago Laight <santilaight@Santiagos-MacBook-Pro.local>
2021-05-17 19:00:43 +0200

    cambios esteticos

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let tableScheduled = document.getElementById('table-scheduled');
 let matchday1 = document.getElementById("fecha-1");
 let standings2021 = document.getElementById('standings2021')
 
+
+//RESULTADOS
+
 for(let i=0; i<filteredMatches.length; i++){
     if(filteredMatches[i].status == "FINISHED") {
     let tr= document.createElement("tr");
@@ -24,13 +27,16 @@ for(let i=0; i<filteredMatches.length; i++){
 //     }
 // }
 
- for (let i=0; i<filteredMatches.length;i++){
-     if(filteredMatches[i].matchday == 1 && filteredMatches[i].status =="FINISHED"){
-         let tr=document.createElement("tr");
-         tr.innerHTML = `<td>${filteredMatches[i].awayTeam.name}</td><td>${filteredMatches[i].score.fullTime.awayTeam} - ${filteredMatches[i].score.fullTime.homeTeam} </td> <td>${filteredMatches[i].homeTeam.name}</td>`
-            matchday1.appendChild(tr)
-        }
- }
+
+//MATCHDAY 1 script
+
+//  for (let i=0; i<filteredMatches.length;i++){
+//      if(filteredMatches[i].matchday == 1 && filteredMatches[i].status =="FINISHED"){
+//          let tr=document.createElement("tr");
+//          tr.innerHTML = `<td>${filteredMatches[i].awayTeam.name}</td><td>${filteredMatches[i].score.fullTime.awayTeam} - ${filteredMatches[i].score.fullTime.homeTeam} </td> <td>${filteredMatches[i].homeTeam.name}</td>`
+//             matchday1.appendChild(tr)
+//         }
+//  }
 
 
  /// STANDINGS
@@ -42,6 +48,11 @@ for (let i=0; i<positionTable.length; i++){
     if(positionTable[0].type = "TOTAL"){
         let tr=document.createElement("tr");
         tr.innerHTML = `<td>${positionTable[i].position}</td> <td> ${positionTable[i].team.name}</td><td>${positionTable[i].points}</td><td>${positionTable[i].won}</td><td>${positionTable[i].lost}</td><td>${positionTable[i].draw}</td>`
-        standings2021.appendChild(tr)
+        standings2021.appendChild(tr);
 }
    }
+
+
+
+   //ESTADISTICAS
+
