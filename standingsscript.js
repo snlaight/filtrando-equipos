79733cbae77d15cd5c8bcfa97d256561d6139566
Santiago Laight <santilaight@Santiagos-MacBook-Pro.local>
2021-05-18 10:34:43 +0200

    terminando tabla de standings y agregando css

--- standingsscript.js
+++ standingsscript.js
@@ -6,7 +6,7 @@ let standings2021 = document.getElementById('standings2021')
 for (let i=0; i<positionTable.length; i++){
     if(positionTable[0].type = "TOTAL"){
         let tr=document.createElement("tr");
-        tr.innerHTML = `<td>${positionTable[i].position}</td> <td> ${positionTable[i].team.name}</td><td>${positionTable[i].points}</td><td>${positionTable[i].won}</td><td>${positionTable[i].lost}</td><td>${positionTable[i].draw}</td>`
+        tr.innerHTML = `<td>${positionTable[i].position}</td><td><img class="logo" src="${positionTable[i].team.crestUrl}"></td><td>${positionTable[i].team.name}</td><td>${positionTable[i].won}</td><td>${positionTable[i].lost}</td><td>${positionTable[i].draw}</td><td>${positionTable[i].goalDifference}</td><td>${positionTable[i].goalsFor}</td><td>${positionTable[i].goalsAgainst}</td><td>${positionTable[i].points}</td><td>${positionTable[i].form}</td>`
         standings2021.appendChild(tr);
 }
    }
