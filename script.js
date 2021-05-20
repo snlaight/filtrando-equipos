728c205215e73e7a959e946221734c2d8f0a3e31
Santiago Laight <santilaight@Santiagos-MacBook-Pro.local>
2021-05-20 12:00:26 +0200

    agregando leyenda

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 let filteredMatches = matches[0].matches;
-
-
 console.log(filteredMatches[0]);
 
 let tableBody= document.getElementById('table-body');
 let tableScheduled = document.getElementById('table-scheduled');
-let matchday1 = document.getElementById("fecha-1");
 let standings2021 = document.getElementById('standings2021')
 
 
@@ -40,17 +37,16 @@ for(let i=0; i<filteredMatches.length; i++){
 
 
  /// STANDINGS
+//  let positionTable = standings[0].standings[0].table;
+//  console.log(positionTable);
 
- let positionTable = standings[0].standings[0].table;
- console.log(positionTable);
-
-for (let i=0; i<positionTable.length; i++){
-    if(positionTable[0].type = "TOTAL"){
-        let tr=document.createElement("tr");
-        tr.innerHTML = `<td>${positionTable[i].position}</td> <td> ${positionTable[i].team.name}</td><td>${positionTable[i].points}</td><td>${positionTable[i].won}</td><td>${positionTable[i].lost}</td><td>${positionTable[i].draw}</td>`
-        standings2021.appendChild(tr);
-}
-   }
+// for (let i=0; i<positionTable.length; i++){
+//     if(positionTable[0].type = "TOTAL"){
+//         let tr=document.createElement("tr");
+//         tr.innerHTML = `<td>${positionTable[i].position}</td> <td> ${positionTable[i].team.name}</td><td>${positionTable[i].points}</td><td>${positionTable[i].won}</td><td>${positionTable[i].lost}</td><td>${positionTable[i].draw}</td>`
+//         standings2021.appendChild(tr);
+// }
+//    }
 
 
 
