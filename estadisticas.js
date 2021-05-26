c7147a8fb48d83ef4f8e6107f06348af0c1b0700
Santiago Laight <santilaight@Santiagos-MacBook-Pro.local>
2021-05-26 19:36:48 +0200

    funcion de pintarTabla agregada, y tabla de estadisticas (con todos los equipos) agregada.

--- estadisticas.js
+++ estadisticas.js
@@ -1,7 +1,9 @@
-//VARIABLES
+//VARIABLES GLOBALES
 let goalAverage = document.getElementById("goalAverage2021");
 let table = standings[0].standings[0].table;
 let newStats = [];
+
+
 
 //FUNCIONES
 let estadisticas = (teams) => {
@@ -12,27 +14,46 @@ let estadisticas = (teams) => {
     let partidosEquipo = teams[i].playedGames;
     let promedioGol = golesEquipo / partidosEquipo;
     let promediGolRedondado = round(promedioGol);
-    let equipoNuevo = {name: name,logoID:logoID, goalsFor: golesEquipo, PG:partidosEquipo ,avg:promediGolRedondado};
-    newStats.push(equipoNuevo)
+    let equipoNuevo = {
+      name: name,
+      logoID: logoID,
+      goalsFor: golesEquipo,
+      PG: partidosEquipo,
+      avg: promediGolRedondado,
+    };
+    newStats.push(equipoNuevo);
   }
-  
-  let sortedStats = newStats.sort((a,b) => {
-      return b.avg -a.avg  
+  let sortedStats = newStats.sort((a, b) => {
+    return b.avg - a.avg;
   });
-  console.log(sortedStats)
+  console.log(sortedStats);
+};
+console.log(newStats);
+let round = (num, decimales = 2) => {
+  var signo = num >= 0 ? 1 : -1;
+  num = num * signo;
+  if (decimales === 0) return signo * Math.round(num);
+  num = num.toString().split("e");
+  num = Math.round(
+    +(num[0] + "e" + (num[1] ? +num[1] + decimales : decimales))
+  );
+  num = num.toString().split("e");
+  return signo * (num[0] + "e" + (num[1] ? +num[1] - decimales : -decimales));
 };
 
-let round = (num, decimales = 2) =>{
-    var signo = num >= 0 ? 1 : -1;
-    num = num * signo;
-    if (decimales === 0) return signo * Math.round(num);
-    num = num.toString().split("e");
-    num = Math.round(
-      +(num[0] + "e" + (num[1] ? +num[1] + decimales : decimales))
-    );
-    num = num.toString().split("e");
-    return signo * (num[0] + "e" + (num[1] ? +num[1] - decimales : -decimales));
+let pintarTablaPromedio = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td><img class="logo" src="https://crests.football-data.org/${array[i].logoID}.svg"></td>
+    <td>${array[i].name}</td><td>${array[i].PG}</td>
+    <td>${array[i].avg}</td>`;
+
+    goalAverage.appendChild(tr);
   }
+};
+
 
 //DECLARACION FUNCIONES
 estadisticas(table);
+pintarTablaPromedio(newStats);
